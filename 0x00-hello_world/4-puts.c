473d8d426b37509209ed0787645aae45f17fdad3
tewa92 <tewachewsaleigizer@gmail.com>
2023-03-04 15:14:58 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * Write a C program that prints exactly
+ *Programming is like building a multilingual puzzle, followed by a new line.
+ * Use the function puts
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
