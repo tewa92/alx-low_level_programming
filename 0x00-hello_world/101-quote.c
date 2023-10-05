5df901075432370910eec3b6ed2d30011c5cea64
tewa92 <tewachewsaleigizer@gmail.com>
2023-10-05 12:17:17 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include<unistd.h>
 /**
  * main - the main function
- * Return: value is 1 
+ * Return: value is 1
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
