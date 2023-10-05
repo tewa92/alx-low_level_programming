292ef42aeef343364b8383150ef33e83209ddbf7
tewa92 <tewachewsaleigizer@gmail.com>
2023-10-05 11:04:57 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - main of the function
+ * Return: must be 0 always when successfull
+ *\
+int main(void)
+{
+	printf("size of a char:%d byte(s)\n", sizeof(char));
+	printf("size of an int:%d byte(s)\n", sizeof(int));
+	printf("size of a long int:%d byte(s)\n", sizeof(long int));
+	printf("size of a long long int:%d byte(s)\n", sizeof( long long int));
+	printf("size of a float:%d byte(s)\n," sizeof(float));
+	return(0);
+}
