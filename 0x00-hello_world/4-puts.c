b9c085ec184664f3f203e29e55562ebced4c699a
tewa92 <tewachewsaleigizer@gmail.com>
2023-10-05 07:23:09 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
-int main(void)
+/**
+ *main -Entry point 
+ * 
+ *Retrun always 0(success)
+ */
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle")
 	retrun (0);
