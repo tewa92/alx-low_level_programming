45d13d82e98ea3ff2d0045e327582beaab896a2c
tewa92 <tewachewsaleigizer@gmail.com>
2023-10-05 11:48:01 -0700

    code (Intel syntax) of a C code and save it in an output file.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - function which is main
+ * Return: value is always 0
+ *\
+ 	int main(void)
+	write(2,
+	("and that piece of art is useful" - Dora Korpar, 2015-10-19")
+	return(1);
