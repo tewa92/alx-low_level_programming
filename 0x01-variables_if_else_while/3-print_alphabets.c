b074cde98a07502df696763fc1041260edd09292
tewa92 <tewachewsaleigizer@gmail.com>
2023-10-06 13:13:03 -0700

    print lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - show the lower and upper case
+ *
+ * Return:The value is Always 0 (When Success)
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
