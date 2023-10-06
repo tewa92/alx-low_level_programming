06affaf8a344cae7516e0c3156602e16548ebf38
tewa92 <tewachewsaleigizer@gmail.com>
2023-10-06 13:09:09 -0700

    display_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - display the alphabet.
+ *
+ * Return: The value is  Always 0 (when success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
