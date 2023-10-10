08d50bca9943f45fbb2e3645b6ea98363d4909de
tewa92 <tewachewsaleigizer@gmail.com>
2023-10-10 12:31:52 -0700

    print_alphabe_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print lower case 10 times 
+ * Return: is always 0
+ */
+
+void print_alphabet_x10(void)
+{
+
+char ch ;
+int i;
+i = 1;
+while (i <= 10)
+{
+for ( ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+i++;
+}
+
+
+}
