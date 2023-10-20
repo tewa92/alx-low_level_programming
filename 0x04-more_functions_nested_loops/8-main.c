69fcd95c548c91112802410cea18b211dd603da8
tewa92 <tewachewsaleigizer@gfmail.com>
2023-10-20 13:25:59 -0700

    prints a square

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - Check The Code Given
+*
+* Return: Value Is Always 0.
+*/
+int main(void)
+{
+print_square(2);
+print_square(10);
+print_square(0);
+return (0);
+}
