9e20561dd70f05762a27419aff4252bef8b73129
tewachewsaleigizer@gmail.com <tewachewsaleigizer@gmail.com>
2024-02-02 00:28:18 -0800

    Hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Create a hash table with the specified size.
- * @sizee: Size of the array for the hash table.
+ * hash_table_create - creates a hash table
+ * @sizee: size of the array
  *
- * Return: Pointer to the newly created hash table
+ * Return: pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int sizee)
 {
